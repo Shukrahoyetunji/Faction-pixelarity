8622cbd91e3c3a677ff47776d8714246e496b2d4
Shukrahoyetunji <shoyetunji@gmail.com>
2022-05-19 13:48:15 +0100

    My first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelectorAll("carousel").forEach(carousel=>{
+    const slide = carousel.querySelectorAll("slide");
+    const bottonhtml = Array.from(items,() =>{
+        return '<span class="carousel_button"></span>'
+    });
+    carousel.insertAdjacentHTML("beforeend",''
+    <div class="carousel">
+        
+    </div>
+    );
+});
